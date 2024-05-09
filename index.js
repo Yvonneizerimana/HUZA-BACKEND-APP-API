61ef9c378f6e42af2be86918556203395d3ffe5a
yvonne IZERIMANA <izerimanayvonne510@gmail.com>
2024-05-09 12:48:25 +0200

    done with admin

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import  express  from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv"
-dotenv.config();
+import dotenv from "dotenv";
+dotenv.config()
+import adminRouter from './routes/index.js';
+import cookieParser from "cookie-parser";
+import swaggerUi from "swagger-ui-express";
+import documentation from "./doc/documentation.js"
+
 const app = express()
 const port = process.env.PORT 
 const db = process.env.MONGODB_URI
@@ -9,10 +14,12 @@ const db = process.env.MONGODB_URI
 
 //middleware
 app.use(express.json())
-
+app.use(cookieParser())
 
 //routes
-
+app.use('/api', adminRouter);
+app.use('/api-doc', swaggerUi.serve);
+app.use('/api-doc', swaggerUi.setup(documentation));
 
 //connect database
 
