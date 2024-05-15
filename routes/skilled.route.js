2080ab71f1e2336e3cd870c86904a0d3a7c95900
yvonne IZERIMANA <izerimanayvonne510@gmail.com>
2024-05-15 06:23:16 +0200

    Profile Swagger

--- routes/skilled.route.js
+++ routes/skilled.route.js
@@ -1,6 +1,6 @@
 import skilledController from "../controllers/skilled.controller.js";
-import profileController from "../controllers/profile.controller.js";
-import upload from "../middlewares/fileupload.js";
+// import profileController from "../controllers/profile.controller.js";
+// import upload from "../middlewares/fileupload.js";
 import Validation from "../middlewares/Validation.js";
 import { Router } from "express";
 const skilledroute = Router()
@@ -12,5 +12,5 @@ skilledroute.post("/verify",skilledController.ValidateOpt)
 skilledroute.post("/forgotPassword",skilledController.ForgotPassword)
 skilledroute.post("/resetPassword",skilledController.ResetPassword)
 
-skilledroute.post("/createProfile",upload.single("file"),profileController.create)
+// skilledroute.post("/createProfile",upload,profileController.create)
 export default skilledroute
