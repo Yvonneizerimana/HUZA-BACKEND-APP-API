720f3309347e562653149fddd48354235ba93ca9
yvonne IZERIMANA <izerimanayvonne510@gmail.com>
2024-05-15 09:03:18 +0200

    users check

--- routes/profile.route.js
+++ routes/profile.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import profileController from '../controllers/profile.controller.js';
+import authCheck from "../utils/authCheck.js"
 
 const router = express.Router();
 
-router.post('/createProfile', profileController.create);
-router.put('/update/:id', profileController.update);
-router.delete('/delete/:id', profileController.delete);
+router.post('/createProfile', authCheck.skilled,profileController.create);
+router.put('/update/:id', authCheck.skilled, profileController.update);
+router.delete('/delete/:id', authCheck.skilled, profileController.delete);
 
 export default router;
