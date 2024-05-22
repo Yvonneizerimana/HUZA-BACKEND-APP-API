f5e322e8e11cda4c12a486cf5d009533d606f181
BIENAIMEE Marie Reine <mbienaimee@alustudent.com>
2024-05-22 15:39:31 +0200

    project

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -75,62 +75,8 @@ const profileController = {
     }
   },
 
-  // create: async (req, res) => {
-  //   upload(req, res, async (err) => {  // Changed from upload.multiple to just upload
-  //     if (err) {
-  //       console.log(err);
-  //       return res.status(400).json({ message: err.message });
-  //     }
-  //     // console.log(req.files)
 
-  //     try {
-  //       const { firstName, lastName, email, Address, education,resume, nationalID, certificate, photo, category } = req.body;
 
-  //       // Extract file paths from req.files (assuming successful upload)
-  //       const resumePath = req.files['documents[resume]'] ? req.files['documents[resume]'][0].path : null;
-  //       const nationalIDPath = req.files['documents[nationalID]'] ? req.files['documents[nationalID]'][0].path : null;
-  //       const certificatePath = req.files['documents[certificate]'] ? req.files['documents[certificate]'][0].path : null;
-  //       const photoPath = req.files['documents[photo]'] ? req.files['documents[photo]'][0].path : null;
-
-  //       const newProfile = await Profile.create({
-  //         firstName,
-  //         lastName,
-  //         email,
-  //         Address: {
-  //           country: Address.country,
-  //           province: Address.province,
-  //           district: Address.district,
-  //           sector: Address.sector,
-  //         },
-  //         education: {
-  //           school: education.school,
-  //           major: education.major,
-  //           didyoufinished: education.didyoufinished,
-  //           timeofstudy: education.timeofstudy,
-  //         },
-  //         documents: {
-  //           resume: resumePath,
-  //           nationalID: nationalIDPath,
-  //           certificate: certificatePath,
-  //           photo: `/upload/`+photoPath,
-  //         },
-  //         category,
-  //       });
-
-  //       // await newProfile.save(); // Save the new profile to the database
-
-  //       res.status(200).json({
-  //         status: "success",
-  //         profile: newProfile,
-  //       });
-  //     }  catch (error) {
-  //       res.status(500).json({
-  //         status: "error",
-  //         message: error.message,
-  //       });
-  //     }
-  //   });
-  // },
 
   delete: async (req, res) => {
     try {
